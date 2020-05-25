5499331906e8c866854369fd7367ca9ee2d2d535
annaponoch <60112879+annaponoch@users.noreply.github.com>
2020-05-25 23:02:18 +0300

    Update index.js

--- index.js
+++ index.js
@@ -12,9 +12,9 @@ let your_tasks = [];
 if(localStorage.getItem('your_tasks') && localStorage.getItem('your_tasks').length != 0) {
 let sorted = JSON.parse(localStorage.getItem("your_tasks"));
 sorted.forEach(function(elem, index) {
-		element = document.createElement('li')
-		element.textContent = elem
-		ul.appendChild(element)
+element = document.createElement('li')
+element.textContent = elem
+ul.appendChild(element)
 	});
 }
 //
@@ -26,7 +26,6 @@ let sorted = arr.map(elem => elem.textContent).sorting()
 arr[index].textContent = elem
 )
 }
-//
     buttn.onclick = function() {
 if(input.value.length != 0 && input.value.replace(/\s/g,"") != "") {
 let li = document.createElement('li');
@@ -34,9 +33,7 @@ li.textContent = input.value;
 ul.appendChild(li);
 input.value = '';
 sorting();
-	}
-}
-//
+}}
 input.addEventListener("keyup", function(event) {
 if (event.keyCode === 13) {
 event.preventDefault();
