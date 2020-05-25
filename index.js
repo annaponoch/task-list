e9c1f9eb587a752b9acfe24ce57bbcaacc36a014
annaponoch <60112879+annaponoch@users.noreply.github.com>
2020-05-25 23:01:40 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
-let container = document.querySelector('#container');
-let input = document.createElement('input');
-let btn = document.createElement('button');
 let ul = document.createElement('ul');
+let input = document.createElement('input');
+let buttn = document.createElement('button');
+let container = document.querySelector('#container');
 
-btn.textContent = 'Add';
+buttn.textContent = 'Add';
 //
-      container.appendChild(ul);
-      container.appendChild(input);
-      container.appendChild(btn);
-//
+container.appendChild(ul);
+container.appendChild(input);
+container.appendChild(buttn);
 let your_tasks = [];
 if(localStorage.getItem('your_tasks') && localStorage.getItem('your_tasks').length != 0) {
-	let sorted = JSON.parse(localStorage.getItem("your_tasks"));
-	sorted.forEach(function(elem, index) {
+let sorted = JSON.parse(localStorage.getItem("your_tasks"));
+sorted.forEach(function(elem, index) {
 		element = document.createElement('li')
 		element.textContent = elem
 		ul.appendChild(element)
@@ -24,17 +23,16 @@ let arr = Array.from(document.querySelectorAll('li'));
 let sorted = arr.map(elem => elem.textContent).sorting()
        localStorage.setItem("your_tasks", JSON.stringify(sorted));
        sorted.forEach((elem, index) =>
-		arr[index].textContent = elem
-	)
+arr[index].textContent = elem
+)
 }
 //
-    btn.onclick = function() {
+    buttn.onclick = function() {
 if(input.value.length != 0 && input.value.replace(/\s/g,"") != "") {
 let li = document.createElement('li');
-   li.textContent = input.value;
-   ul.appendChild(li);
-   input.value = '';
-
+li.textContent = input.value;
+ul.appendChild(li);
+input.value = '';
 sorting();
 	}
 }
@@ -42,6 +40,6 @@ sorting();
 input.addEventListener("keyup", function(event) {
 if (event.keyCode === 13) {
 event.preventDefault();
-btn.click();
+buttn.click();
   }
 });
